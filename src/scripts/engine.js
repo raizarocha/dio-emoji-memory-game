857cd4d79664e1fff8bba1221aa964c0afe5b253
raizarochaw <raizarocha8@gmail.com>
2024-02-17 01:02:54 -0300

    feat: criando os cards dinamicamente

---
+++ src/scripts/engine.js
@@ -0,0 +1,38 @@
+const emojis = [
+  "🍕",
+  "🍕",
+  "🍔",
+  "🍔",
+  "🍟",
+  "🍟",
+  "🍣",
+  "🍣",
+  "🧁",
+  "🧁",
+  "☕",
+  "☕",
+  "🍧",
+  "🍧",
+  "🍭",
+  "🍭"
+];
+
+// guarda os elementos que forem sendo abertos
+let openCards = [];
+
+// aplica para cada elemento o peso de 2 ou -1 (de acordo com a condição); 
+// o elemento com peso -1 vai para trás e o elemento com peso 2 vai para frente;
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+
+// coloca os cards dinamicamente na página
+for (let i=0; i < emojis.length; i++) {
+  // para cada elemento do array emojis, cria uma div (que representa visualmente um card)
+  let card = document.createElement("div");
+  // com a class item
+  card.className = "item";
+  // o elemento será pego na shuffleEmojis de acordo com a posição
+  card.innerHTML = shuffleEmojis[i];
+  // faz o apêndice do card na div principal (".game")
+  document.querySelector(".game").appendChild(card);
+}
